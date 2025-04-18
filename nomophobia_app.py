f15d1f16753e1ad8a3489516334716803f443f6e
HELHPSY <honayneelhassani@gmail.com>
2025-04-19 00:22:45 +0100

    Update nomophobia_app.py

--- nomophobia_app.py
+++ nomophobia_app.py
@@ -1,9 +1,4 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import plotly.graph_objects as go
-import plotly.express as px
-from datetime import datetime
 from streamlit_cookies_manager import EncryptedCookieManager
 # Page config
 st.set_page_config(
